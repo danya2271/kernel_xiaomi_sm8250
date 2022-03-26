51d1e2df41f837bb93868b16e1c941f391097d3d
Alexander Winkowski <dereference23@outlook.com>
2022-03-26 16:22:08 +0000

    cpuidle: lpm-levels: Remove duplicated code
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    [ Tashar02: Adapt to https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet/commit/151f46d3a047b0e41a1efc4d44f8b00afc8bcae8 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -740,9 +740,7 @@ static bool psci_enter_sleep(struct lpm_cpu *cpu, int idx, bool from_idle)
 	 */
 
 	if (!idx) {
-		stop_critical_timings();
 		cpu_do_idle();
-		start_critical_timings();
 		return true;
 	}
 
@@ -756,11 +754,7 @@ static bool psci_enter_sleep(struct lpm_cpu *cpu, int idx, bool from_idle)
 	affinity_level = PSCI_AFFINITY_LEVEL(affinity_level);
 	state_id += power_state + affinity_level + cpu->levels[idx].psci_id;
 
-	stop_critical_timings();
-
 	success = !arm_cpuidle_suspend(state_id);
-
-	start_critical_timings();
 
 	if (from_idle && cpu->levels[idx].use_bc_timer)
 		tick_broadcast_exit();
@@ -799,8 +793,6 @@ static int lpm_cpuidle_enter(struct cpuidle_device *dev,
 
 	cluster_unprepare(cpu->parent, cpumask, idx, true, 0, success);
 	cpu_unprepare(cpu, idx, true);
-	dev->last_residency = ktime_us_delta(ktime_get(), start);
-	local_irq_enable();
 	return idx;
 }
 
