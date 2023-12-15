339fdc2ee790b20ec121b89d13ab87f5498f248a
danya2271 <danya2271@.yandex.ru>
2023-12-15 12:15:34 +0500

    Fix errors

--- drivers/soc/qcom/msm_bus/msm_bus_arb_rpmh.c
+++ drivers/soc/qcom/msm_bus/msm_bus_arb_rpmh.c
@@ -1639,7 +1639,7 @@ static int update_request_adhoc(uint32_t cl, unsigned int index)
 	if (pdata->alc)
 		ret = update_client_alc(client, log_transaction, index);
 	else {
-		ret = update_client_paths(client, index);
+		ret = update_client_paths(client, log_transaction, index);
 	}
 	if (ret) {
 		pr_err("%s: Err updating path\n", __func__);
