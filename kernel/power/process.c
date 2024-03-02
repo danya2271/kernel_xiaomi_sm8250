9e1d2c4b231884f72bde371a64a9c84197334dd3
danya2271 <danya2271@.yandex.ru>
2024-03-02 14:11:03 +0500

    kernel: power: very fast deepsleep

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -26,8 +26,8 @@
 /*
  * Timeout for stopping processes
  */
-unsigned int __read_mostly freeze_timeout_msecs =
-        IS_ENABLED(CONFIG_ANDROID) ? MSEC_PER_SEC : 20 * MSEC_PER_SEC;
+unsigned int __read_mostly freeze_timeout_msecs = 250;
+
 
 static int try_to_freeze_tasks(bool user_only)
 {
