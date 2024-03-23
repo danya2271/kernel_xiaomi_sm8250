f02f1b47599cebe9565a1300205ca1101c8eccf0
danya2271 <danya2271@.yandex.ru>
2024-03-24 01:59:07 +0500

    treewide: Optimize frequencies for best battery n speed

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1118,18 +1118,22 @@ static int sugov_init(struct cpufreq_policy *policy)
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
 		tunables->up_rate_limit_us = 5000;
 		tunables->down_rate_limit_us = 5000;
+		tunables->hispeed_load = 50;
+		tunables->hispeed_freq = 1344000;
 	}
 
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
 		tunables->up_rate_limit_us = 16000;
 		tunables->down_rate_limit_us = 1000;
 		tunables->hispeed_load = 50;
+		tunables->hispeed_freq = 1382000;
 	}
 
         if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
                 tunables->up_rate_limit_us = 35000;
                 tunables->down_rate_limit_us = 0;
 				tunables->hispeed_load = 40;
+				tunables->hispeed_freq = 1401600;
         }
 
 	policy->governor_data = sg_policy;
