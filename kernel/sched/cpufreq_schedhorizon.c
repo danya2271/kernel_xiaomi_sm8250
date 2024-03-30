b2eb1be62a59c5908f204354550a8e169f41d7d8
danya2271 <danya2271@.yandex.ru>
2024-03-31 01:05:49 +0500

    kernel-sched: schedhorizon: Increase delay for frequencies higher than efficient
    Gave a little better battery

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -22,10 +22,10 @@ static unsigned int default_efficient_freq_lp[] = {1344000, 1612800};
 static u64 default_up_delay_lp[] = {4 * NSEC_PER_MSEC, 20 * NSEC_PER_MSEC};
 
 static unsigned int default_efficient_freq_hp[] = {1382000, 1766400};
-static u64 default_up_delay_hp[] = {20 * NSEC_PER_MSEC, 60 * NSEC_PER_MSEC};
+static u64 default_up_delay_hp[] = {20 * NSEC_PER_MSEC, 120 * NSEC_PER_MSEC};
 
 static unsigned int default_efficient_freq_pr[] = {1401200};
-static u64 default_up_delay_pr[] = {100 * NSEC_PER_MSEC};
+static u64 default_up_delay_pr[] = {180 * NSEC_PER_MSEC};
 
 #define DEFAULT_RTG_BOOST_FREQ_LP 1612800
 #define DEFAULT_RTG_BOOST_FREQ_HP 1766400
