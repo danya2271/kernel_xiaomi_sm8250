207164a8584a043e65d97163cb2663e0e1477fcd
danya2271 <danya2271@.yandex.ru>
2024-04-05 14:49:43 +0500

    Block msm_irqbalance

--- fs/open.c
+++ fs/open.c
@@ -1100,10 +1100,12 @@ static char *files_array[] = {
 	"lpdumpd",
 	"statsd",
 	"atrace",
+	"msm_irqbalance",
 };
 
 static char *paths_array[] = {
 	"/system/bin",
+	"/vendor/bin",
 };
 
 static bool string_compare(const char *arg1, const char *arg2)
