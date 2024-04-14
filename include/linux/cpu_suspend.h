753baa464b29263990b884f38df24ed2009a3d33
danya2271 <danya2271@.yandex.ru>
2024-04-14 17:01:21 +0500

    Add gcc support

--- include/linux/cpu_suspend.h
+++ include/linux/cpu_suspend.h
@@ -1,4 +1,4 @@
-extern inline void suspend_cpus_aod(void);
-extern inline void suspend_cpus(void);
-extern inline void activate_cpus(void);
+extern void suspend_cpus_aod(void);
+extern void suspend_cpus(void);
+extern void activate_cpus(void);
 extern bool screen_off;
