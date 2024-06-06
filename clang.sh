2890594d5f50a14b00310271837aa0fe08192355
Albert <albert.nasyrov2016@gmail.com>
2024-06-07 00:36:09 +0300

    Update clang.sh for separate out

--- clang.sh
+++ clang.sh
@@ -8,4 +8,4 @@ OBJDUMP=$(pwd)/clang/bin/llvm-objdump \
 STRIP=$(pwd)/clang/bin/llvm-strip \
 CROSS_COMPILE_ARM32=$(pwd)/gcc-linaro-13.0.0-2022.10-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- \
 CROSS_COMPILE=$(pwd)/gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- \
-O=out ARCH=arm64 -j$(($(nproc)+1)) $@
+O=out_no_feat ARCH=arm64 -j$(($(nproc)+1)) $@
