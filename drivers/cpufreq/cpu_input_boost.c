a018a483d75db4e4397aea44b639e616792d460b
danya2271 <dany2271@yandex.ru>
2024-08-18 22:43:55 +0500

    drivers: cpufreq_input_boost: Limit frequencies to minimum when screen off

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -273,6 +273,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 	/* Unboost when the screen is off */
 	if (test_bit(SCREEN_OFF, &b->state)) {
 		policy->min = get_idle_freq(policy);
+		policy->max = get_idle_freq(policy);
 		return NOTIFY_OK;
 	}
 
