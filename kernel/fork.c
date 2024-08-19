0df83f760fc2b181f2d0c64f76159a67f20249f2
danya2271 <dany2271@yandex.ru>
2024-08-19 17:58:49 +0500

    kernel: fork,cgroup-v1: Also boost gpu frequency on app launch

--- kernel/fork.c
+++ kernel/fork.c
@@ -99,6 +99,7 @@
 #include <linux/cpu_input_boost.h>
 #include <linux/simple_lmk.h>
 
+#include <linux/gpu_input_boost.h>
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -2391,6 +2392,7 @@ long _do_fork(unsigned long clone_flags,
 	if (task_is_zygote(current)) {
                 cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+		gpu_input_boost_kick_max(150);
 	}
 
 	/*
