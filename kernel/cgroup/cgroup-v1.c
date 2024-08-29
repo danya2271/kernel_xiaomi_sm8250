efc17d1c20c9b7af0f4fe77e11ef3f7c74143311
danya2271 <dany2271@yandex.ru>
2024-08-29 15:11:57 +0500

    kernel: cgroup: Fix compilation error

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -13,8 +13,8 @@
 #include <linux/delayacct.h>
 #include <linux/pid_namespace.h>
 #include <linux/cgroupstats.h>
-#include <linux/gpu_input_boost.h>
 #include <linux/binfmts.h>
+#include <linux/cpu.h>
 
 #include <linux/cpu_input_boost.h>
 #include <linux/gpu_input_boost.h>
