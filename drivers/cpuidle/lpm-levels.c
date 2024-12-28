a85d6838fbd4d3a340d2de588bb86f39845c4c45
danya2271 <dany2271@yandex.ru>
2024-12-28 15:13:26 +0500

    Revert "cpuidle: lpm-levels: Remove duplicated code"
    This reverts commit a1deb387b46222f9a65d019d055e5c231a6d65d3.

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -669,8 +669,10 @@ static int psci_enter_sleep(struct lpm_cpu *cpu, int idx, bool from_idle)
 	 */
 
 	if (!idx) {
+		stop_critical_timings();
 		cpu_do_idle();
-		return 0;
+		start_critical_timings();
+		return true;
 	}
 
 	if (from_idle && cpu->levels[idx].use_bc_timer) {
@@ -684,8 +686,11 @@ static int psci_enter_sleep(struct lpm_cpu *cpu, int idx, bool from_idle)
 	affinity_level = PSCI_AFFINITY_LEVEL(affinity_level);
 	state_id += power_state + affinity_level + cpu->levels[idx].psci_id;
 
-	ret = !arm_cpuidle_suspend(state_id);
-	success = (ret == 0);
+	stop_critical_timings();
+
+	success = !arm_cpuidle_suspend(state_id);
+
+	start_critical_timings();
 
 	if (from_idle && cpu->levels[idx].use_bc_timer)
 		tick_broadcast_exit();
@@ -730,6 +735,8 @@ exit:
 
 	cluster_unprepare(cpu->parent, cpumask, idx, true, end_time, success);
 	cpu_unprepare(cpu, idx, true);
+	dev->last_residency = ktime_us_delta(ktime_get(), start);
+	local_irq_enable();
 	return idx;
 }
 
